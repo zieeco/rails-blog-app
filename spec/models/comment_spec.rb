17a0e4f8ab4f1b4ab40ee606dd987ab11006194e
zieeco <zieecosam@gmail.com>
2022-07-08 20:52:59 +0100

    Test the Comment model

---
+++ spec/models/comment_spec.rb
@@ -0,0 +1,30 @@
+require 'rails_helper'
+
+RSpec.describe Comment, type: :model do
+  describe 'Testing the Comment model' do
+    before(:example) do
+      @user = User.new(name: 'zieeco', photo: 'photo_link', bio: 'Developer From Nigeria')
+      @post = Post.new(author: @user, title: 'My Title', comments_counter: 0, likes_counter: 0)
+      @comment = Comment.new(text: 'First comment', author_id: @user.id, post_id: @post.id)
+    end
+
+    it 'if text is present' do
+      @comment.text = nil
+      expect(@comment).to_not be_valid
+    end
+
+    it 'test if author_id is integer' do
+      @comment.author_id = 'string'
+      expect(@comment).to_not be_valid
+    end
+
+    it 'test if post_id is integer' do
+      @comment.post_id = 'string'
+      expect(@comment).to_not be_valid
+    end
+
+    it 'test if comment counter is updated' do
+      expect(@post.comments_counter).to_not eq 5
+    end
+  end
+end

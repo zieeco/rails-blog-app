032e2e741cac45e282c13a05d6664787d066a3a3
zieeco <zieecosam@gmail.com>
2022-07-15 18:22:42 +0100

    Update the post controller

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,12 +1,38 @@
 class PostsController < ApplicationController
   def index
     @user = User.find(params[:user_id])
-    @posts = @user.posts
+    @posts = @user.posts.includes(:comments)
   end
 
   def show
+    @user = User.find(params[:user_id])
     @post = Post.find(params[:id])
     @comments = @post.comments
     @likes = @post.likes
+  end
+
+  def new
+    @user = current_user
+    @post = Post.new
+  end
+
+  def create
+    @user = current_user
+    @post = @user.posts.new(post_params)
+
+    if @post.save
+      @post.update_post_counter
+      flash[:notice] = 'Your post was created successfully'
+      redirect_to user_path(@user), notice: 'Your post was created successfully!'
+    else
+      flash[:alert] = 'sorry, something went wrong!'
+      render :new
+    end
+  end
+
+  private
+
+  def post_params
+    params.require(:post).permit(:title, :text)
   end
 end

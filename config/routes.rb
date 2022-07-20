c6a5b20646555ceedc32b0cee16185ec871a6e72
zieeco <zieecosam@gmail.com>
2022-07-20 02:46:37 +0100

    Generate the devise

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,5 @@
 Rails.application.routes.draw do
+  devise_for :users
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
   resources :users, only: %i[index show] do
     resources :posts, only: %i[index show new create] do
